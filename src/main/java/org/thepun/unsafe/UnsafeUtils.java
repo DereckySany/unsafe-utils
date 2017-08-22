691956864d47a9436dd642ff39c256d1e55101b2
mgariev <thepun599@gmail.com>
2017-08-22 14:22:47 +0300

    local maven added

--- src/main/java/org/thepun/unsafe/UnsafeUtils.java
+++ src/main/java/org/thepun/unsafe/UnsafeUtils.java
@@ -1,11 +1,24 @@
 package org.thepun.unsafe;
 
+import java.lang.reflect.Field;
+
 import sun.misc.Unsafe;
 
 public class UnsafeUtils {
 
+    private static final Unsafe INSTANCE;
+    static {
+        try {
+            Field singleoneInstanceField = Unsafe.class.getDeclaredField("theUnsafe");
+            singleoneInstanceField.setAccessible(true);
+            INSTANCE = (Unsafe) singleoneInstanceField.get(null);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static Unsafe getUnsafe() {
-        return null;
+        return INSTANCE;
     }
 
 }
